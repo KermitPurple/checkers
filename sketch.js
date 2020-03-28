52dd5ad83a7234dc3b03dc46902150c8cc778cb7
Shane McDonough <shanemcdman@gmail.com>
2020-03-28 05:05:13 -0700

    Create draw board

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
 function setup(){
 	createCanvas(600,600);
-	background(0)
 }
 
 function draw(){
+	drawBoard()
+}
+
+function drawBoard(){
+	let scl = width/8;
+	let color
+	for(let i = 0; i < 8; i++){
+		for(let j = 0; j < 8; j++){
+			if((j + i) % 2 == 1){
+				color = 20
+			} else {
+				color = 200
+			}
+			fill(color);
+			beginShape();
+			vertex(j * scl, i * scl);
+			vertex((j + 1) * scl, i * scl);
+			vertex((j + 1) * scl, (i + 1) * scl);
+			vertex(j * scl, (i + 1) * scl);
+			endShape(CLOSE);
+		}
+	}
 }
 
 function mousePressed(){
-	circle(mouseX, mouseY, 20);
 }
