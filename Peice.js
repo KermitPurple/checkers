09da5606f1766feae8efeed42b0ac236057e8fb1
Shane McDonough <shanemcdman@gmail.com>
2020-04-11 22:46:20 -0700

    ValidMove only allows forward movement

--- Peice.js
+++ Peice.js
@@ -17,17 +17,18 @@ class Peice{
 		ellipse(this.pos.x * scl + scl/16, this.pos.y * scl + scl/16 , scl - scl/8)
 	}
 
-validMove(potential){
-	if(!exists(potential)){
-		for(let i = -1; i <= 1; i += 2){ 
-			for(let j = -1; j <= 1; j += 2){ 
-				if(this.pos.x + j == potential.x && this.pos.y + i == potential.y){
-						return true
+	validMove(potential){
+		if(!exists(potential)){
+			for(let i = -1; i <= 1; i += 2){ 
+				for(let j = -1; j <= 1; j += 2){ 
+					if(this.pos.x + j == potential.x && this.pos.y + i == potential.y){
+						if((i > 0 && turn == 1) || (i < 0 && turn == 0)){
+							return true
+						}
+					}
 				}
 			}
 		}
+		return false
 	}
-	return false
-}
-
 };
